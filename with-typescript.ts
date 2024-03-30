0454924d4f4e98769e24615ac752d0b1d6f8aff3
arhaamwanii <arhamwani765@gmail.com>
2024-03-30 22:06:08 +0530

    typescript basic

---
+++ with-typescript.ts
@@ -0,0 +1,11 @@
+function add(a, b) {
+  return a + b;
+}
+
+const result = add('2', 5);
+
+console.log(result);
+
+//  standard web project 
+// well we will get into  is later
+// animating the react apps
